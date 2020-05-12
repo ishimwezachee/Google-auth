696a00cbea0fb959058ecac1b20389c20763aec1
zachee <zacheeishimwe@gmail.com>
2020-05-12 10:57:44 +0200

    create a model database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const authRoutes = require("./routes/auth-routes");
 const passportSetup = require('./config/passport-setup.js');
+const mongoose = require('mongoose')
 //set up view engine 
 
 app.set('view engine','ejs');
@@ -13,6 +14,19 @@ app.get('/',(req,res)=>{
 res.render('home');
 })
 
+
+
+
 app.listen(3400,()=>{
     console.log("app listening to 3400 ");
 })
+
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/googleAuth',
+{ 
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology:true
+  },()=> console.log('Database successfully connected')
+  )
