c9f0c03d7ba9ce606a04108ae5554671e6fe71fe
zachee <zacheeishimwe@gmail.com>
2020-05-12 11:59:19 +0200

    store google's data in databasse

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 const Keys = require('./key');
+const User = require("../models/user"); 
 passport.use(
     new GoogleStrategy({
     // options for the google stat
@@ -8,7 +9,21 @@ passport.use(
     clientID:Keys.google.clientID,
     clientSecret:Keys.google.clientSecret
 },(accessToken,refreshToken,profile,done)=>{
-    // passport callback function 
-   console.log(profile)
+// check if user already exists 
+User.findOne({googleId:profile.id}).then((currentUser)=>{
+if(currentUser){
+    // already have user 
+    console.log('user is:',currentUser)
+}else{
+    // if not , createa a new user
+    new User({
+        username:profile.displayName,
+        googleId:profile.id
+    }).save().then((newUser)=>{
+        console.log("new user created:" + newUser)
+    })
+}
+})
+
 })
 )
