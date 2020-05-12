834be09a95f724eeb8d2d10cb8d38c5e64d13e8d
zachee <zacheeishimwe@gmail.com>
2020-05-12 07:49:56 +0200

    lesson-4

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-
+const authRoutes = require("./routes/auth-routes")
 //set up view engine 
 
 app.set('view engine','ejs');
+
+// set up routes  
+app.use("/auth",authRoutes)
 // create home route 
 app.get('/',(req,res)=>{
 res.render('home');
